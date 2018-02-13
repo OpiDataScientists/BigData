655f0ad1a6f6b40d7b27f0e8bb34ba6ab8d1de40
Zoghlami Alaeddine <azoghlami@adinpi.intra>
2018-02-13 15:39:28 +0100

    1er commit

---
+++ bigData.R
@@ -0,0 +1,44 @@
+ptm=proc.time() #start time 
+B16DOSSIER <- read.csv("~/rgit/bigdata/B16_DOSSIER_DATA_TABLE.csv") # download from OPI server
+proc.time()-ptm #160 secondes 
+format(object.size(B16DOSSIER),"Mb") #"676.8 Mb"
+
+
+
+##%######################################################%##
+#                                                          #
+####                    FF PACKAGE                      ####
+#                                                          #
+##%######################################################%##
+
+#préciser la direction des ff files
+options(fftempdir = "~/rgit/bigdata/")
+library(ffbase)
+library(ff)
+ptm=proc.time()
+DOSSIER.ff=read.csv.ffdf(file="B16_DOSSIER_DATA_TABLE.csv",VERBOSE=TRUE,first.rows=-1,header=T) 
+proc.time()-ptm #140 secondes
+format(object.size(DOSSIER.ff),"Mb") #496.2 Mb
+
+
+
+#chaque variable dans un fichier 7 variable ==> 7 fichers ff
+list.files(path="~/rgit/bigdata")
+#save file
+save.ffdf("DOSSIER.ff",dir="saved_DOSSIER.ff")
+
+
+#Remove and then load it 
+rm(list=ls())
+gc()
+ptm=proc.time()
+load.ffdf("~/rgit/bigdata/saved_DOSSIER.ff")
+proc.time()-ptm  #3 secondes
+dossier=as.data.frame(DOSSIER.ff)
+##%######################################################%##
+#                                                          #
+####                 BIG MEMORY PACKAGE                 ####
+#                                                          #
+##%######################################################%##
+
+
